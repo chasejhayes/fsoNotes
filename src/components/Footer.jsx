96e510410c22ea1ce44c145fa01ff27264e8e276
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2026-04-30 12:45:04 -0500

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+    const footerStyle = {
+        color: "green",
+        fontStyle: "italic"
+    }
+return(
+    <div style={footerStyle}>
+        <br />
+        <p>
+            Note app, Department of Chase, University of Hayes 2026
+        </p>
+
+    </div>
+
+)
+}
+
+export default Footer
